f8d876955594db582289e986c91a2a1156e905a5
mkanegon <33151120+mkanegon@users.noreply.github.com>
2017-11-10 19:50:56 +0900

    kadai3
    問3

---
+++ kadai3/src/kadai3/Main.java
@@ -0,0 +1,16 @@
+package kadai3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int a, k;
+
+		a = 5;
+
+		for(k = 0; k <= a; k++) {
+		System.out.print(k );
+	    }
+	}
+}
