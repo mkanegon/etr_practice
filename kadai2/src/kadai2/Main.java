d6a7dfbd7bfe6289f99df0d2e973edf037219389
mkanegon <33151120+mkanegon@users.noreply.github.com>
2017-11-10 19:43:06 +0900

    javakadai2
    問2

---
+++ kadai2/src/kadai2/Main.java
@@ -0,0 +1,18 @@
+package kadai2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int x, y;
+
+        x = 4;
+        y = 2;
+
+        System.out.println("足し算："+(x + y));
+        System.out.println("引き算："+(x - y));
+        System.out.println("掛け算："+(x * y));
+        System.out.println("割り算："+(x / y));
+	}
+
+}
