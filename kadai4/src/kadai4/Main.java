2bc348b0ec01f823b05f4d3fe5ad82bef3bdf79d
mkanegon <33151120+mkanegon@users.noreply.github.com>
2017-11-10 19:55:13 +0900

    kadai4
    問4

---
+++ kadai4/src/kadai4/Main.java
@@ -0,0 +1,18 @@
+package kadai4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int a;
+
+        a = 3;
+
+        if(a % 2 == 0) {
+        	System.out.println("偶数");
+        }else {
+        	System.out.println("奇数");
+        }
+	}
+
+}
